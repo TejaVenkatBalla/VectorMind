fc5a6f7f76603963021a8991dbf1f05a2444b13c
TejaVenkatBalla <tejavenkatballa@gmail.com>
2025-06-07 17:08:09 +0530

    feat: configure CORS settings

--- home/settings.py
+++ home/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY",temp_SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -57,6 +59,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
