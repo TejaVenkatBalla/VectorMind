0fe16f7f53de73d4eef817a6485fd52b7ce3fa92
TejaVenkatBalla <tejavenkatballa@gmail.com>
2025-07-28 22:50:21 +0530

    fix: update API_BASE_URL to correct production endpoint

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = "https://ragchat.hopto.org";
+export const API_BASE_URL = "https://ragchat.duckdns.org";
 
-//export const API_BASE_URL = "http://backend:8000";
+//export const API_BASE_URL = "http://localhost:8000";
 
-export const API_BASE_URL = "https://ec2-13-126-230-218.ap-south-1.compute.amazonaws.com";
+//export const API_BASE_URL = "https://ec2-13-126-230-218.ap-south-1.compute.amazonaws.com";
